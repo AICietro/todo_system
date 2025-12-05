4a05c8c1e8486a55f608d27ba19fcfbc1e693e8c
Cietro <2693331910@qq.com>
2025-12-05 18:24:19 +0800

    modified:   app/database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-engine = create_engine("sqlite:///./test.db")
+engine = create_engine("sqlite:///./app/test.db")
 
 
 class Base(DeclarativeBase):
